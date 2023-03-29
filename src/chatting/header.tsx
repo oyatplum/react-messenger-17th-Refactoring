709531dabd5ddda9e1805c7d952246853aa0ecd3
yeji <oyatpeach@naver.com>
2023-03-29 09:54:56 +0900

    feat: compose header content

--- src/chatting/header.tsx
+++ src/chatting/header.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import ToggleUser from './../user/ToggleUser';
+import userList from './../json/users.json';
 
 function header() {
-  return <div>header</div>;
+  return (
+    <Head>
+      <Button>back</Button>
+      <ToggleUser />
+      <ToggleUser />
+    </Head>
+  );
 }
+
+const Head = styled.div`
+  display: flex;
+  height: 60px;
+  align-items: center;
+`;
+const Button = styled.button`
+  border: none;
+  background: rgb(205, 222, 241);
+  border-radius: 20px;
+  font-size: 15px;
+  height: 1.5rem;
+  margin-left: 10px;
+`;
 
 export default header;
---
+++ src/user/ToggleUser.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ToggleUser() {
+  return <div>ToggleUser</div>;
+}
+
+export default ToggleUser;
