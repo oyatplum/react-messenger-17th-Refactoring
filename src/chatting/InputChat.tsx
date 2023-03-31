1384aa45744541744cca13c32acd28f09061ac39
yeji <oyatpeach@naver.com>
2023-03-31 18:22:38 +0900

    feat: modify input component

--- src/chatting/InputChat.tsx
+++ src/chatting/InputChat.tsx
@@ -20,8 +20,9 @@ function InputChat() {
     setSelectedId(1);
   }
 
-  const submitText = (e: React.FormEvent<HTMLButtonElement>) => {
+  const submitText = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log('submit?');
     if (text.trim()) {
       let newChat: Message = {
         userNum: selectedId,
@@ -34,39 +35,47 @@ function InputChat() {
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('change');
     setText(e.target.value);
   };
 
   return (
-    <Enter>
-      <InputText onSubmit={() => submitText} />
-      <EnterButton onChange={() => handleChange} value={text}>
-        전송
-      </EnterButton>
-    </Enter>
+    // <Enter>
+    <InputText onSubmit={() => submitText}>
+      <InputForm onChange={handleChange} value={text} />
+      <EnterButton>전송</EnterButton>
+    </InputText>
+    // </Enter>
   );
 }
-const Enter = styled.form`
-  backgorund-color: blue;
-  height: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-const InputText = styled.input`
+// const Enter = styled.form`
+//   backgorund-color: blue;
+//   height: 80px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
+const InputForm = styled.input`
   border: none;
   background-color: rgb(205, 222, 241);
   border-radius: 20px;
   height: 35px;
   font-size: 15px;
-  width: 80%;
+  width: 70%;
 `;
 const EnterButton = styled.button`
   border: none;
   background: rgb(205, 222, 241);
   border-radius: 20px;
-  margin-left: 10px;
+  margin-left: 15px;
   font-size: 15px;
+`;
+const InputText = styled.div`
+  backgorund-color: blue;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default InputChat;
